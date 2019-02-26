39b79b652ad7ad4fb3c5b4160c7e50d1614cdac3
davismont <davismont@d8739450-1e93-4ef4-a0af-c327d92816ff>
2019-02-26 20:34:32 +0000

    MATLAB examples for parallel processing updated
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@2527 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Version8/Distrib/Examples/Parallel_Processing/MATLAB/Example_1/Main_Script.m
+++ Version8/Distrib/Examples/Parallel_Processing/MATLAB/Example_1/Main_Script.m
@@ -1,26 +1,28 @@
+% This example works with OpenDSS version 8.5.4.1 and later
 clc;
 [DSSStartOK, DSSObj, DSSText] = DSSStartup;
 DSSCircuit      =   DSSObj.ActiveCircuit;
 DSSText.Command =   'ClearAll';             % Clears all instances of OpenDSS-PM
-DSSText.Command =   'Set Parallel=Yes';             % Clears all instances of OpenDSS-PM
+DSSText.Command =   'Set Parallel=No';      % Deactivates the parallel features
 
 DSSParallel     =   DSSCircuit.Parallel;    % Habdler for Parallel processing functions
 CPUs            =   DSSParallel.NumCPUs;    % Gets how many CPUs this PC has
-% By default one actor is created by default, if you want more than one
+% By default one actor is created, if you want more than one
 % parallel instance you will have to create them. Try to leave at least
 % One CPU available to handle the rest of windows, otherwise will block
 % Everything
-for i=1:CPUs-1,
-    if i ~= 1,
-        DSSParallel.CreateActor; % Creates additonal actors
-    end;
-    DSSText.Command =   'compile (C:\Program Files\OpenDSS\EPRITestCircuits\ckt5\Master_ckt5.DSS)';
-    DSSCircuit.Solution.Solve;    
-    DSSParallel.Wait;   % for the first solve, it is needed to wait before creating other actor
-    DSSText.Command =   'set mode=Time stepsize=1h number=16000';
-end;
+disp('Creating Actors');
+DSSText.Command =   'compile "C:\Program Files\OpenDSS\EPRITestCircuits\ckt5\Master_ckt5.DSS"';
+DSSText.Command =   'set maxiterations=1000 maxcontroliter=1000';   % Just in case
+DSSCircuit.Solution.Solve;                      % Solves Actor 1
+DSSText.Command =   ['Clone ',int2str(CPUs-2)]; %Creates the other actors
+DSSText.Command =   'set ActiveActor=*';        %activates all actors to send commands concurrently
+DSSText.Command =   'set mode=Time stepsize=1h number=16000';
+% to send commands to each actor separately you need to select each actor
+% indepedently
+DSSText.Command =   'set ActiveActor=1'; % Go back to actor 1
+DSSText.Command =   'Set Parallel=Yes';   % Activates the parallel features
 % Now the actors are solved
-
 DSSCircuit.Solution.SolveAll;
 
 pause(0.1); 
@@ -36,4 +38,4 @@ while BoolStatus == 0,
     pause(0.5);  %  A little wait to not saturate the Processor  
 end;
 disp('Simulation finished by all the actors');
-
+ 
