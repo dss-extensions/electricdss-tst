33a487e48572f0a653277a916e4e5754b03b43ed
davismont <davismont@d8739450-1e93-4ef4-a0af-c327d92816ff>
2019-10-18 13:39:40 +0000

    Adding another python example
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@2726 d8739450-1e93-4ef4-a0af-c327d92816ff

---
+++ Version8/Distrib/Examples/Parallel_Processing/Python/Example_2/Example_2.py
@@ -0,0 +1,63 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import win32com.client
+from win32com.client import makepy
+import sys
+import gc
+import numpy as np
+import time
+from os import system, name 
+
+sys.argv = ["makepy", "OpenDSSEngine.DSS"]
+makepy.main()
+DSSObj = win32com.client.Dispatch("OpenDSSEngine.DSS")
+DSSText = DSSObj.Text
+DSSCircuit = DSSObj.ActiveCircuit
+DSSSolution = DSSCircuit.Solution
+DSSParallel = DSSCircuit.Parallel
+DSSBus=DSSCircuit.ActiveBus
+DSSCtrlQueue=DSSCircuit.CtrlQueue
+DSSObj.Start(0)
+NCPUs=DSSParallel.NumCPUs
+print(' total number of NCPUs', NCPUs )
+
+DSSText.Command='ClearAll'
+DSSText.Command='set parallel=No'
+
+EndArray=[] # for checking when all the actors are done
+yDelta=8760/(NCPUs-1)
+ActorCPU =[]
+
+for x in range(0, (NCPUs-1)):
+    print('Core Number',x)
+    if x != 0:
+        DSSParallel.CreateActor()
+    DSSText.Command='compile "C:/Program Files/OpenDSS/EPRITestCircuits/ckt5/master_ckt5.DSS"'
+    DSSText.Command='Solve'
+    if x==NCPUs-1:
+        yDelta=8760-(NCPUs-2)*yDelta
+    DSSText.Command='set mode=yearly totaltime=0 number=' + str(yDelta) + ' hour=' + str(x*yDelta)
+    EndArray.append(1)
+            
+DSSText.Command='set parallel=Yes'
+DSSText.Command='SolveAll'
+BoolStatus = False
+time.sleep(1);
+while BoolStatus == False:
+    ActorStatus = list(DSSParallel.ActorStatus);
+    BoolStatus = ActorStatus == EndArray
+    ActorProgress=DSSParallel.ActorProgress
+    system('cls') 
+    print('BoolStatus = ',BoolStatus,' ActorStatus = ',ActorStatus,' ActorProgress = ', ActorProgress)
+    for i in range(1,(NCPUs-1)):
+        DSSParallel.ActiveActor = i;
+        CHour = DSSSolution.dblHour;
+        print('Actor Time(hours)',CHour);
+    time.sleep(0.5)
+    
+print('Simulation finished');
+
